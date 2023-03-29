ee3b2df4131d503588762e9cf41eb3f43607df43
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-29 22:43:22 +0530

    The programme file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            UC_1SimpleLinkedList num = new UC_1SimpleLinkedList();
+            num.Addlast(56);
+            num.Addlast(30);
+            num.Addlast(70);
+            num.Display();
+            Console.ReadLine();
+        }
+    }
+}
